fba261f6202a1d07bf99461b10b45a3381947396
Keith Casey <keith@caseysoftware.com>
2015-10-20 23:24:25 -0500

    setup

---
+++ creds-dist.php
@@ -0,0 +1,12 @@
+<?php
+
+$google_access = '{"access_token":"big ugly token","token_type":"Bearer","expires_in":3600,"refresh_token":"another token","created":1445399592}';
+
+$uber_api_key  = 'the oauth result';
+
+$calendars = array('calendar1',     // personal
+    'calendar2',                    // my meetups
+    'calendar3',                    // tripit
+    'calendar4',                    // work
+    'calendar5'                     // facebook
+);
